2b5859250964a18b533040117c3778b76e5814c1
Bobby Wang <bobbyquennell@gmail.com>
2020-08-03 23:57:00 +1000

    add a payment serverless worker

--- services/sls-payment-worker/src/index.ts
+++ services/sls-payment-worker/src/index.ts
@@ -1,26 +1,20 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
-import axios from 'axios';
-// const url = 'http://checkip.amazonaws.com/';
+
 let response;
 
 exports.worker = async (
   event: APIGatewayProxyEvent,
 ): Promise<APIGatewayProxyResult> => {
   try {
-    const result = await axios({
-      method: 'get',
-      url: 'https://jsonplaceholder.typicode.com/todos/1',
-    });
     const inboundMsg = JSON.parse(event?.body);
     console.log(
-      `received message ===> ${inboundMsg?.name} said: ${inboundMsg?.message}`,
+      `sls-payment-worker ===> received payment request: ${inboundMsg?.paymentRequest}`,
     );
 
     response = {
       statusCode: 200,
       body: JSON.stringify({
-        message: `Hi ${inboundMsg?.name ?? 'there'}, I am Peggy`,
-        additionalData: result.data,
+        message: `Payment Success`,
       }),
     };
   } catch (err) {
