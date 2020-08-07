e02c2872452c71c4d3954a765e6dd07d16f2f30a
Bobby Wang <bobbyquennell@gmail.com>
2020-08-07 17:38:43 +1000

    update Order Event Worker
    - handle OrderRequested Event

--- services/sls-order-worker/src/index.ts
+++ services/sls-order-worker/src/index.ts
@@ -1,19 +1,21 @@
-import { SQSEvent, SQSRecord, SQSHandler } from 'aws-lambda';
+import { SQSEvent, SQSHandler, SNSMessage } from 'aws-lambda';
 import { OrderEvent, OrderEventType } from './types';
 import orderHandler from './handler';
-export const worker: SQSHandler = async (message: SQSEvent): Promise<void> => {
-  const parser = (record: SQSRecord): OrderEvent => {
-    return JSON.parse(record.body);
-  };
-  const bodies: OrderEvent[] = message.Records.map(parser);
+
+export const worker: SQSHandler = async (event: SQSEvent): Promise<void> => {
+  const bodies: SNSMessage[] = event.Records.map((record) =>
+    JSON.parse(record.body),
+  );
+
   await Promise.all(
-    bodies.map((body: OrderEvent) => {
-      switch (body.eventType) {
+    bodies.map((body) => {
+      const orderMessage: OrderEvent = JSON.parse(body.Message);
+      switch (orderMessage.eventType) {
         case OrderEventType.OrderRequested:
-          return orderHandler(body);
+          return orderHandler(orderMessage);
         default:
           throw new Error(
-            `Unsupported event type received: "${body.eventType}"`,
+            `Unsupported event type received: "${orderMessage.eventType}"`,
           );
       }
     }),
